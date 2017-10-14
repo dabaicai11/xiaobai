122e48e11137dea91f8dec43adfeadd7291c9f46
luoyuhang <907334706@qq.com>
2017-10-14 16:13:57 +0800

    2

--- mavabc1/src/main/java/com/dzkj/mavabc1/App.java
+++ mavabc1/src/main/java/com/dzkj/mavabc1/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello bay!" );
         System.out.println( "Hello World!dddddddddddddddd" );
+        System.out.println( "Hello World!rennfkjad" );
     }
 }
